4f8423353e7eaa6bc25bbf87178ba43343f3bc42
Mateusz Karwacki <mateusz.karwacki@onwelo.com>
2019-08-01 09:21:02 +0200

    a

--- src/main/java/com/herokusmallapp/controlers/HerokuController.java
+++ src/main/java/com/herokusmallapp/controlers/HerokuController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HerokuController {
-    
+
     @GetMapping
     public String go(){
         return "dupa";
