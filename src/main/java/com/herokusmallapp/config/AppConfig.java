ee8009edc2b6948a14411fb2d1ce63fffcf55912
Mateusz Karwacki <mateusz.karwacki@onwelo.com>
2019-08-01 14:54:26 +0200

    pp

--- src/main/java/com/herokusmallapp/config/AppConfig.java
+++ src/main/java/com/herokusmallapp/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.herokusmallapp.config;
 
 
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -40,6 +42,14 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/notFound").setViewName("forward:/index.html");
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return container -> {
+            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
+                    "/notFound"));
+        };
     }
 
 }
