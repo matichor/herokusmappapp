5f8d95dd3a73755abdb50488685ca7bd474ea30d
Mateusz Karwacki <mateusz.karwacki@onwelo.com>
2019-08-01 09:15:44 +0200

    first

---
+++ src/main/java/com/herokusmallapp/HerokusmallappApplication.java
@@ -0,0 +1,13 @@
+package com.herokusmallapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokusmallappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokusmallappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/herokusmallapp/controlers/HerokuController.java
@@ -0,0 +1,17 @@
+package com.herokusmallapp.controlers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HerokuController {
+    
+    @GetMapping
+    public String go(){
+        return "dupa";
+    }
+
+
+}
